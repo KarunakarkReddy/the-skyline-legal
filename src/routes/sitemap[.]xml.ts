b134c1a515fa9f2d0bbc9072ea10367b6db0375d
kaarunakar <reddykkarunakar3@gmail.com>
2026-07-27 12:39:19 +0530

    Fix sitemap for Google indexing

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -3,22 +3,50 @@ import type {} from "@tanstack/react-start";
 import { partners } from "@/lib/our-team";
 import { practiceAreas } from "@/lib/site";
 
-const BASE_URL = "";
+const BASE_URL = "https://www.theskylinelegal.in";
 
 export const Route = createFileRoute("/sitemap.xml")({
   server: {
     handlers: {
       GET: async () => {
         const paths = [
-          "/", "/about", "/partners", "/practice-areas", "/services",
-          "/testimonials", "/gallery", "/faqs", "/contact", "/book",
-          "/privacy-policy", "/terms",
-          ...partners.map((p) => `/partners/${p.slug}`),
+          "/",
+          "/about",
+          "/our-team",
+          "/practice-areas",
+          "/gallery",
+          "/faqs",
+          "/contact",
+          "/book",
+          "/privacy-policy",
+          "/terms",
+
+          ...partners.map((p) => `/our-team/${p.slug}`),
+
           ...practiceAreas.map((p) => `/practice-areas/${p.slug}`),
         ];
-        const urls = paths.map((p) => `  <url><loc>${BASE_URL}${p}</loc><changefreq>monthly</changefreq></url>`).join("\n");
-        const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n</urlset>`;
-        return new Response(xml, { headers: { "Content-Type": "application/xml", "Cache-Control": "public, max-age=3600" } });
+
+        const urls = paths
+          .map(
+            (p) => `
+  <url>
+    <loc>${BASE_URL}${p}</loc>
+    <changefreq>monthly</changefreq>
+  </url>`
+          )
+          .join("");
+
+        const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+        return new Response(xml, {
+          headers: {
+            "Content-Type": "application/xml",
+            "Cache-Control": "public, max-age=3600",
+          },
+        });
       },
     },
   },
